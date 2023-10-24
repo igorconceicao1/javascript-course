37d1b7b998b37144f4848b84fe486fd86701607a
igorconceicao1 <68449035+igorconceicao1@users.noreply.github.com>
2023-10-24 19:28:57 -0400

    Assign Variable Value Later

--- javascript basics/app.js
+++ javascript basics/app.js
@@ -1,18 +1,17 @@
 // Variable - Most Basic Building Block
 // Variables - Store, Access, Modify === Value
 // Declare, Assignment Operator, Assign Value
+// Assign value later, modify existing
 
 let name1 = "Igor Teste de Nome";
+let address, zip, state;
+
+address = "rua dom cirilo, ";
+zip = "49118450, ";
+state = "MS";
 
 console.log(name1);
-//some code here
+console.log(address, zip, state);
+
+name1 = "igor em minusculo teste 2";
 console.log(name1);
-//some code here
-console.log(name1);
-//some code here
-console.log(name1);
-//some code here
-console.log(name1);
-//some code here
-console.log(name1);
-//some code here
