5e331d197f0a6902ea71c5bef77a3d91a8e9fb77
igorconceicao1 <68449035+igorconceicao1@users.noreply.github.com>
2023-10-24 15:01:27 -0400

    helper methods

--- javascript basics/app.js
+++ javascript basics/app.js
@@ -1,5 +1,13 @@
-document.querySelectorAll(".btn").forEach((item) => {
-  item.addEventListener("click", () => {
-    alert("this is a button test");
-  });
-});
+//document.write('hello world');
+//alert("hello world");
+//console.log("hello world");
+
+document.write("hello world");
+document.write({ name: "igor" });
+
+alert("hello world");
+alert({ name: "igor" });
+
+// sempre melhor usar o console.log
+console.log("hello world");
+console.log({ name: "igor" }); // para objetos é melhor usar o console.log
