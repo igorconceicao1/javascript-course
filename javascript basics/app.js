246020a1519412012e75951b4ad11cd4f3d6a62f
igorconceicao1 <68449035+igorconceicao1@users.noreply.github.com>
2023-10-26 15:31:54 -0400

    data types in js

--- javascript basics/app.js
+++ javascript basics/app.js
@@ -5,28 +5,32 @@
 // const fullName = name + ' ' + lastName;
 // console.log(fullName);
 
-// const number = 4;
-// const number2 = 10;
-// const result = number + number2;
-// console.log(result);
+// Data Types - 7 total
+// Primitive - String, Number, Boolean, Null, Undefined, Symbol
+// Object - Arrays, Functions, Objects
 
-// const value = name - lastName;
-// console.log(value);
+// typeof - operator (typeof variable) (typeof value)
 
-// let number3 = 10;
-// let number4 = 23;
-// // 100 lines
-// number4 = '23';
-// const result2 = number3 + number4;
-// console.log(result2);
+// String
+const text = "some text";
 
-const randomNumber = 13;
-document.querySelector(".form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  let value = document.getElementById("amount").value;
-  value = parseInt(value); //essa linha muda tudo
-  console.log("Input Value Type");
-  console.log(value);
-  console.log("Sum of Two Values");
-  console.log(randomNumber + value);
-});
+// Number
+const number = 45;
+
+// Boolean
+let value1 = true;
+let value2 = false;
+
+// Null
+const result = null;
+
+// Undefined
+let name;
+
+// Symbol(ES6)
+console.log(typeof text);
+console.log(typeof true);
+console.log(typeof number);
+console.log(typeof value1);
+console.log(typeof value2);
+console.log(typeof null);
