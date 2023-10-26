3e18aaed13120da999c349c502088cdda93957d7
igorconceicao1 <68449035+igorconceicao1@users.noreply.github.com>
2023-10-26 09:38:37 -0400

    challanger-01-var

---
+++ challanges/01-variables.js
@@ -0,0 +1,24 @@
+/* 
+## Variables #1
+
+1. create "firstName" and "last_name" variables
+2. assign your values
+3. create "address" variable and assign "main street"
+   value to it
+4. re-assign address to "first street" later
+5. log all values in the console
+*/
+
+const firstName = "Igor";
+const last_name = "Da Conceicao Silva Delgado";
+
+let address = "main street";
+console.log(address);
+
+console.log(firstName);
+console.log(last_name);
+
+address = "first street";
+
+console.log(firstName, last_name);
+console.log(address);
