5fdadf3369b0545f1aa59f45b59b27a2b55dcbc5
igorconceicao1 <68449035+igorconceicao1@users.noreply.github.com>
2024-01-16 18:30:02 -0700

    arrays properties and methods

--- javascript basics/app.js
+++ javascript basics/app.js
@@ -1,11 +1,41 @@
-// Template Literals - ES6+
-// Backtick characters `` - above tab
-// Interpolation ${} - insert expression(value)
+// Array Properties and Methods
+let names = ["john", "bobo", "barry", "olga", "ben"];
+const lastNames = ["pepper", "onion", "banana"];
 
-const name = "igor";
-const age = 28;
-const setence = "Hey its me " + name + ", and I have " + age + " years old.";
-console.log(setence);
+//length - comprimento do array
+console.log(names.length);
+console.log(names[names.length - 1]); // ultimo elemento by index
 
-const value = `Hey it's me ${name}, and I have ${age} years old`;
-console.log(value);
+// concat
+const allNames = names.concat(lastNames);
+console.log(allNames);
+// reverse
+console.log(allNames.reverse());
+
+//unshift - Isso significa que o array terá um novo elemento no índice 0, e os índices dos elementos existentes serão incrementados.
+allNames.unshift("susy");
+allNames.unshift("anna");
+console.log(allNames);
+
+//shift - Isso significa que o elemento no índice 0 será removido, e os índices dos elementos restantes serão decrementados.
+allNames.shift();
+allNames.shift();
+allNames.shift();
+allNames.shift();
+console.log(allNames);
+
+//push -  Isso significa que o novo elemento será inserido no final do array.
+allNames.push("susy");
+console.log(allNames);
+
+//pop - Isso significa que o elemento no final do array será removido.
+allNames.pop();
+// allNames.pop();
+// allNames.pop();
+// allNames.pop();
+console.log(allNames);
+
+// splice - mutates original array - Remove três elementos do array allNames, começando do índice 0. Esses elementos removidos são retornados pelo método splice.
+const specificNames = allNames.splice(0, 3);
+console.log(specificNames);
+console.log(allNames);
